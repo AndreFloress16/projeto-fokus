0d196988e0267b9794b017d9aebf117541fd6796
AndreFloress16 <andreluizsf16@gmail.com>
2025-02-17 16:00:20 -0300

    quase

--- script-crud.js
+++ script-crud.js
@@ -31,9 +31,11 @@ function criarElementoTarefa(tarefa) {
 
         botao.onclick = () => {
            const novaDescricao = prompt("Qual é o novo nome da tarefa?") 
-           paragrafo.textContent = novaDescricao
-           tarefa.descricao = novaDescricao
-           atualizarTarefas()
+           if (novaDescricao) {
+            paragrafo.textContent = novaDescricao
+            tarefa.descricao = novaDescricao
+            atualizarTarefas()
+           }
         }
 
         imagemBotao.setAttribute('src', '/imagens/edit.png')
